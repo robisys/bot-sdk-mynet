0ed52aa6c5f634c61c8fc894ecb658eebcb97fa8
Robert P <robisys@users.noreply.github.com>
2017-10-13 21:05:04 +0200

    Add files via upload

--- gen_cert.sh
+++ gen_cert.sh
@@ -10,12 +10,6 @@ CSR_NAME="myservercert"
 KEYSTORE_PASSWORD="pw123456"
 KEYSTORE_FILE="keystore.jks"
 
-# openssl genrsa -out server.key 4096
-# openssl req -new -key server.key -out csr.pem
-# openssl x509 -req -days 7300 -in csr.pem -signkey server.key -out server.crt
-# openssl rsa -in server.key -pubout -out pubkey.pem
-
-
 mkdir -p $CERTS_DIR
 openssl genrsa -out $CERTS_DIR/privkey.pem 4096
 openssl req -new -subj "/C=$CSR_COUNTRY/ST=$CSR_STATE/L=$CSR_LOCALITY/O=$CSR_ORGANISATION/CN=$CSR_COMMON" -key $CERTS_DIR/privkey.pem -out $CERTS_DIR/csr.pem
@@ -24,6 +18,5 @@ openssl rsa -in $CERTS_DIR/privkey.pem -pubout -out $CERTS_DIR/pubkey.pem
 openssl pkcs12 -export -name $CSR_NAME -in $CERTS_DIR/cert.pem -inkey $CERTS_DIR/privkey.pem -out $CERTS_DIR/keystore.p12 -passout pass:$KEYSTORE_PASSWORD
 keytool -importkeystore -noprompt -destkeystore $CERTS_DIR/$KEYSTORE_FILE -srckeystore $CERTS_DIR/keystore.p12 -srcstorepass $KEYSTORE_PASSWORD -storepass $KEYSTORE_PASSWORD -srcstoretype pkcs12 -alias $CSR_NAME
 
-echo "server.key save"
-copy $CERTS_DIR/privkey.pem  $CERTS_DIR/server.key
+copy $CERTS_DIR/privkey.pem $CERTS_DIR/server.key
 
