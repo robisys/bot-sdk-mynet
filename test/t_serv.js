6fb483ac581bbe0b4c8abc681bf69db440b7a3b2
Robert P <robisys@users.noreply.github.com>
2017-10-15 23:25:45 +0200

    Add files via upload

---
+++ test/t_serv.js
@@ -0,0 +1,30 @@
+/*
+ *  key.pem  server.key
+ *  cer.pem  server.crt
+ 
+https://docs.nodejitsu.com/articles/HTTP/servers/how-to-create-a-HTTPS-server/
+ 
+openssl genrsa -out key.pem
+openssl req -new -key key.pem -out csr.pem
+openssl x509 -req -days 9999 -in csr.pem -signkey key.pem -out cert.pem
+rm csr.pem
+ 
+ 
+ 
+ 
+ */
+
+var https = require('https');
+var fs = require('fs');
+
+var options = {
+  key: fs.readFileSync('server.key'),
+  cert: fs.readFileSync('server.crt')
+};
+
+var a = https.createServer(options, function (req, res) {
+  res.writeHead(200);
+  res.end("hello world\n");
+}).listen(8000);
+
+
