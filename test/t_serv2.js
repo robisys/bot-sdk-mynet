ac9085d79fd0ebe5dbf0d1c8cbc057d8d3f7c767
Robert P <robisys@users.noreply.github.com>
2017-10-23 17:03:59 +0200

    Update t_serv2.js

--- test/t_serv2.js
+++ test/t_serv2.js
@@ -13,43 +13,7 @@ rm csr.pem
  
  
  */
-// http://node.readthedocs.io/en/latest/api/https/
-/*
-var https = require('https');
-var options = {
-  hostname: 'encrypted.google.com',
-  port: 443,
-  path: '/',
-  method: 'GET'
-};
-var req = https.request(options, function(res) {
-  console.log("statusCode: ", res.statusCode);
-  console.log("headers: ", res.headers);
-  res.on('data', function(d) {
-    process.stdout.write(d);
-  });
-});
-req.end();
-req.on('error', function(e) {
-  console.error(e);
-});
-*/
 
-/*
-
-var https = require('https');
-var fs = require('fs');
-
-var options = {
-  key: fs.readFileSync('server.key'),
-  cert: fs.readFileSync('server.crt')
-};
-
-var a = https.createServer(options, function (req, res) {
-  res.writeHead(200);
-  res.end("hello world\n");
-}).listen(8000);
-*/
 
 // /*
 // https://www.hacksparrow.com/express-js-https-server-client-example.html
@@ -80,48 +44,3 @@ app.get('/ho', function(req, res) {
     res.send('HO!');
 });
 
-/*
-
-Hmmm what be those .pem files? Those are the associated SSL certificates for your HTTPS web server. Read up: How to create SSL certificates for Node.js.
-And here is the code for the TLS client:
-var PORT = 8000;
-var HOST = 'localhost';
-var tls = require('tls');
-var fs = require('fs');
-var options = { ca: [ fs.readFileSync('./cert.pem') ] };
-var client = tls.connect(PORT, HOST, options, function() {
-    if (client.authorized) {
-        
-        console.log('CONNECTED AND AUTHORIZED\n');
-        
-        client.on('close', function() {
-            console.log('SOCKET CLOSED\n');
-            process.exit();
-        });
-        
-        process.stdin.pipe(client);
-        process.stdin.resume();
-        
-        // Time to make some request to the server
-        // We will write straight to the socket, but recommended way is to use a client library like 'request' or 'superagent'
-        client.write('GET /hey HTTP/1.1\r\n');
-        client.write('\r\n');
-        
-        client.write('POST /ho HTTP/1.1\r\n');
-        client.write('\r\n');
-    }
-    else {
-        console.log('AUTH FAILED\n');
-        process.exit();
-    }
-});
-client.setEncoding('utf8');
-client.on('data', function(data) {
-    console.log('-------------');
-    console.log(data);
-});
- */
-// http://book.mixu.net/node/ch10.html
-
-//https://forge.autodesk.com/blog/enable-https-local-nodejs
-//https://github.com/adamenagy/data.management-nodejs-integration.egnyte
