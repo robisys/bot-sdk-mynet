a7a06b0806ca449a01b106a6835c707b67cc138a
Robert P <robisys@users.noreply.github.com>
2017-10-26 17:21:13 +0200

    Update t_serv1.js

--- test/t_serv1.js
+++ test/t_serv1.js
@@ -1,55 +1,48 @@
-var express = require('express');
-var app = express();
-//var app = require('./server/server');
-
-// Setting up local https support
-var fs = require('fs');
-var https = require('https');
-
-var HOST ='localhost';
-var PORT = 443;
-
-var options = {
-    key: fs.readFileSync('server.key'),
-    cert: fs.readFileSync('server.crt'),
-    passphrase: '<the password you used for the certificate>',
-    requestCert: false,
-    rejectUnauthorized: false
-};
-
 /*
- app.configure(function(){
-   app.use(app.router);
-   });
+ *  key.pem  server.key
+ *  cer.pem  server.crt
+ 
+https://docs.nodejitsu.com/articles/HTTP/servers/how-to-create-a-HTTPS-server/
+ 
+openssl genrsa -out key.pem
+openssl req -new -key key.pem -out csr.pem
+openssl x509 -req -days 9999 -in csr.pem -signkey key.pem -out cert.pem
+rm csr.pem
+ 
+ 
+ 
+ 
+ */
+
+
+// /*
+// https://www.hacksparrow.com/express-js-https-server-client-example.html
+var fs = require('fs');
+var express = require('express');
+var https = require('https');
+var key = fs.readFileSync('./server.key');
+var cert = fs.readFileSync('./server.crt')
+var https_options = {
+    key: key,
+    cert: cert
+};
+var PORT = 8000;
+var HOST = 'localhost';
+app = express();
+/*
+app.configure(function(){
+    app.use(app.router);
+});
 */
-// Start server
-
-var server = https.createServer(options, function(req,res){
-
-res.writeHead(200);
-res.end("hello World\n") ;
-
-}).listen(PORT,HOST);
-    console.log('Server listening on  %s:%s',HOST,PORT);
-
-
+server = https.createServer(https_options, app).listen(PORT, HOST);
+console.log('HTTPS Server listening on %s:%s', HOST, PORT);
 // routes
 app.get('/hey', function(req, res) {
     res.send('HEY!');
 });
-app.post('/ho', function(req, res) {
+app.get('/ho', function(req, res) {
     res.send('HO!');
 });
-
-
-
-
-/*
-var server = https.createServer(options, app).listen(443, function () {
-    console.log('Starting at ' + (new Date()).toString());
-    console.log('Server listening on port ' + server.address().port);
-});
-*/
 
 
 
