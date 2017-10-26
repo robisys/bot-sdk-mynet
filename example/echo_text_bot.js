280f15e9b714e272b7223190d5efd1e5daf611a9
Robert P <robisys@users.noreply.github.com>
2017-10-26 10:30:09 +0200

    Update echo_text_bot.js

--- example/echo_text_bot.js
+++ example/echo_text_bot.js
@@ -15,6 +15,7 @@
  * You should have received a copy of the GNU General Public License
  * along with this program. If not, see http://www.gnu.org/licenses/.
  *
+ * echo_text_bot.js
  */
 
 const path = require('path');
